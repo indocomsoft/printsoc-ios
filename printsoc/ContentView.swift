0a7271250390a40b68c9a696f4dbaf69ed727e34
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-12-05 20:15:00 +0800

    Initial Commit

---
+++ printsoc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  printsoc
+//
+//  Created by Julius on 5/12/19.
+//  Copyright © 2019 Julius. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
